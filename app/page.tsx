875c53d3ee1154083f7387bc6cf1d8fbd092048e
piyush mishra <piyushmishra967@gmail.com>
2024-08-20 17:28:01 +0530

    removed a line

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-cols bg-white  justify-between p-5">
       <div className=" w-full max-w-5xl  justify-between font-mono text-sm">
-        {/* <Image
-          className="w-24 h-24 rounded-full"
-          src="https://example.com/image.jpg"
-          alt="Profile Picture"
-          width={50}
-          height={50}
-        /> */}
         <h1 className="text-3xl text-black font-bold">Welcome to WeebWatch!</h1>
         <p className="text-lg text-black mb-5">
           A web streaming plateform for tracking & watching your favorite anime
